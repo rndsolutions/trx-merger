f963eb78d6ce552366dcc48695cddb19171b6e0d
Karthik Shan <karthik.shanmugasundaram@aon.com>
2018-07-08 01:20:14 -0400

    Added the ability to merge in the references to the local code coverage file(s) assocaited with the test run(s). This does not merge in the actual coverage results themselves. This information can now be used by an external tool to merge the code coverage file(s).

---
+++ TRX_Merger/TrxModel/Anchor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRX_Merger.TrxModel
+{
+	public class Anchor
+	{
+		public string href { get; set; }
+	}
+}
---
+++ TRX_Merger/TrxModel/Collector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRX_Merger.TrxModel
+{
+	public class Collector
+	{
+		public string AgentName { get; set; }
+		public string Uri { get; set; }
+		public string CollectorDisplayName { get; set; }
+		public List<UriAttachment> UriAttachments { get; set; }
+	}
+}
